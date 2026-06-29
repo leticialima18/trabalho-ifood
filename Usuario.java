b0703840d4ca26a4862203f38b53e412806bd46a
leticialima18 <leticialimadias183@gmail.com>
2026-06-29 10:05:29 -0300

    Add Usuario class with user attributes and methods

---
+++ Usuario.java
@@ -0,0 +1,67 @@
+public class Usuario {
+
+    private String nome;
+    private String email;
+    private String telefone;
+    private String endereco;
+    private String senha;
+
+    public Usuario(String nome, String email, String telefone, String endereco, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.endereco = endereco;
+        this.senha = senha;
+    }
+
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    
+    public void exibirDados() {
+        System.out.println("===== Dados do Usuário =====");
+        System.out.println("Nome: " + nome);
+        System.out.println("Email: " + email);
+        System.out.println("Telefone: " + telefone);
+        System.out.println("Endereço: " + endereco);
+    }
+}
